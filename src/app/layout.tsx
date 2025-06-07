e0b49eedcf9bb0d3103e29c711e1374e404385a8
Hunny Jain <hunnyjain711@gmail.com>
2025-06-07 16:23:43 +0530

    Adsense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -168,7 +168,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2950629050940113" crossOrigin="anonymous"></script>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2950629050940113"
+     crossOrigin="anonymous"></script>
       </head>
       <body>{children}</body>
       <Analytics />
